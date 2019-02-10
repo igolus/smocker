e11f7bf93eb34e8dc8c606f5c35d3cf9d0529696
igolus <igolus@gmail.com>
2019-02-11 00:27:37 +0100

    new dev

---
+++ smockerVaadin/src/main/java/com/jenetics/smocker/dao/DaoSingletonLock.java
@@ -0,0 +1,20 @@
+package com.jenetics.smocker.dao;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class DaoSingletonLock {
+	private static ReentrantLock lock = new ReentrantLock();
+
+	public static ReentrantLock getLock() {
+		return lock;
+	}
+	
+	public static void lock() {
+		lock.lock();
+	}
+	
+	public static void unlock() {
+		lock.unlock();
+	}
+	
+}

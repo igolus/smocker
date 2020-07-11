820c2531dae8513062f4ebf2cb830ac929f736c1
igolus <igolus@gmail.com>
2020-07-11 10:54:33 +0200

    Db starter with system property
    rename ant docker target name
    clean root folder

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM jboss/wildfly
-RUN /opt/jboss/wildfly/bin/add-user.sh admin Admin#70365 --silent
-CMD ["/opt/jboss/wildfly/bin/standalone.sh", "-b", "0.0.0.0", "-bmanagement", "0.0.0.0"]
-#ADD https://github.com/igolus/easyApp-demo/blob/master/easyApp-demo-1.0-SNAPSHOT.war /opt/jboss/wildfly/standalone/deployments/
-ADD smocker-1.0-SNAPSHOT.war /opt/jboss/wildfly/standalone/deployments/
-COPY eclipselink-2.7.6.jar /opt/jboss/wildfly/modules/system/layers/base/org/eclipse/persistence/main
-COPY module.xml /opt/jboss/wildfly/modules/system/layers/base/org/eclipse/persistence/main
-USER root
-RUN chown jboss:jboss /opt/jboss/wildfly/standalone/deployments/*
--- smockerVaadin/src/main/java/com/jenetics/smocker/bootstrap/DbStarter.java
+++ smockerVaadin/src/main/java/com/jenetics/smocker/bootstrap/DbStarter.java
@@ -26,86 +26,101 @@ import org.h2.util.StringUtils;
  */
 public class DbStarter implements ServletContextListener {
 
-    private Connection conn;
-    private Server server;
+	private Connection conn;
+	private Server server;
 
 	private Logger logger = Logger.getLogger(DbStarter.class.getName());
-    
-    public void contextInitialized(ServletContextEvent servletContextEvent) {
-        try {
-            org.h2.Driver.load();
 
-            // This will get the setting from a context-param in web.xml if defined:
-            ServletContext servletContext = servletContextEvent.getServletContext();
+	public void contextInitialized(ServletContextEvent servletContextEvent) {
+		try {
+			org.h2.Driver.load();
 
-            // Start the server if configured to do so
-            String serverParams = getParameter(servletContext, "db.tcpServer", null);
-            
-            serverParams += " ";
-            serverParams += "-webAllowOthers";
-            serverParams += " ";
-            serverParams += "-webPort";
-            serverParams += " ";
-            serverParams += "8888";
-            serverParams += " ";
-            serverParams += "-key";
-            serverParams += " ";
-            serverParams += "./smockerPersistant";
-            serverParams += " ";
-            serverParams += "./smockerPersistant";
-            
-            
-            if (serverParams != null) {
-                String[] params = StringUtils.arraySplit(serverParams, ' ', true);
-                server = Server.createTcpServer(params);
-                server.start();
-            }
-            
-            
-            String url = "jdbc:h2:tcp://localhost/./smockerPersistant";
-            String user = getParameter(servletContext, "db.user", "sa");
-            String password = getParameter(servletContext, "db.password", "sa");
-            conn = DriverManager.getConnection(url, user, password);
-            
-            servletContext.setAttribute("connection", conn);
-        } catch (Exception e) {
-            logger.log(Level.SEVERE, "Unable to start H2 db", e);
-        }
-    }
+			// This will get the setting from a context-param in web.xml if defined:
+			ServletContext servletContext = servletContextEvent.getServletContext();
 
-    private static String getParameter(ServletContext servletContext, String key, String defaultValue) {
-        String value = servletContext.getInitParameter(key);
-        return value == null ? defaultValue : value;
-    }
+			// Start the server if configured to do so
+			String serverParams = getParameter(servletContext, "db.tcpServer", null);
 
-    /**
-     * Get the connection.
-     *
-     * @return the connection
-     */
-    public Connection getConnection() {
-        return conn;
-    }
+			serverParams += " ";
+			serverParams += "-webAllowOthers";
+			serverParams += " ";
+			serverParams += "-webPort";
+			serverParams += " ";
+			serverParams += "8888";
+			serverParams += " ";
 
-    public void contextDestroyed(ServletContextEvent servletContextEvent) {
-//        try {
-//            Statement stat = conn.createStatement();
-//            stat.execute("SHUTDOWN");
-//            //stat.close();
-//        } catch (Exception e) {
-//        	logger.log(Level.SEVERE, "Unable to close the statement", e);
-//        }
-//        try {
-//            if (conn != null) {
-//            	conn.close();
-//            }
-//        } catch (Exception e) {
-//        	logger.log(Level.SEVERE, "Unable to close the connection", e);
-//        }
-//        if (server != null) {
-//            server.stop();
-//            server = null;
-//        }
-    }
+			serverParams += "-baseDir";
+			serverParams += " ";
+			serverParams += "C:/temp";
+			serverParams += " ";
+
+			String dbDir = System.getProperty("smocker.dbdir");
+			if (dbDir != null) {
+				serverParams += "-baseDir";
+				serverParams += " ";
+				serverParams += dbDir;
+				serverParams += " ";
+			}
+
+			serverParams += "-key";
+			serverParams += " ";
+
+			serverParams += "./smockerPersistant";
+			serverParams += " ";
+			serverParams += "./smockerPersistant";
+
+
+			if (serverParams != null) {
+				String[] params = StringUtils.arraySplit(serverParams, ' ', true);
+				server = Server.createTcpServer(params);
+				server.start();
+			}
+
+
+			String url = "jdbc:h2:tcp://localhost/./smockerPersistant";
+			String user = getParameter(servletContext, "db.user", "sa");
+			String password = getParameter(servletContext, "db.password", "sa");
+			conn = DriverManager.getConnection(url, user, password);
+
+			servletContext.setAttribute("connection", conn);
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "Unable to start H2 db", e);
+		}
+	}
+
+	private static String getParameter(ServletContext servletContext, String key, String defaultValue) {
+		String value = servletContext.getInitParameter(key);
+		return value == null ? defaultValue : value;
+	}
+
+	/**
+	 * Get the connection.
+	 *
+	 * @return the connection
+	 */
+	public Connection getConnection() {
+		return conn;
+	}
+
+	public void contextDestroyed(ServletContextEvent servletContextEvent) {
+		//        try {
+		//            Statement stat = conn.createStatement();
+		//            stat.execute("SHUTDOWN");
+		//            //stat.close();
+		//        } catch (Exception e) {
+		//        	logger.log(Level.SEVERE, "Unable to close the statement", e);
+		//        }
+		//        try {
+		//            if (conn != null) {
+		//            	conn.close();
+		//            }
+		//        } catch (Exception e) {
+		//        	logger.log(Level.SEVERE, "Unable to close the connection", e);
+		//        }
+		//        if (server != null) {
+		//            server.stop();
+		//            server = null;
+		//        }
+	}
 
 }
